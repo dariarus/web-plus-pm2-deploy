380f0dc0412f96c8a74bce1830fdb9467a87f1f1
dasha <kirena00@mail.ru>
2022-10-28 19:12:05 +0300

    config frontend deploy

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,26 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps : [{
+    name   : "mesto-frontend",
+    script : "./src/index.js"
+  }],
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/dariarus/web-plus-pm2-deploy',
+      path: DEPLOY_PATH,
+      ssh_options: 'StrictHostKeyChecking=no',
+      'pre-deploy-local': `scp -Cr ./build/* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH} && scp ./.env* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'cd frontend && npm i && npm run build',
+    },
+  },
+};
